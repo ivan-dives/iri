08ee766a98b33d7e251db839fe24d988080eb5ac
Ivan Dives <ivan.dives@gmail.com>
2012-04-21 14:41:53 +0300

    Rewrote decode_iri_nocache using urllib.

--- test.sh
+++ test.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+if [ "x$1" == "x-v" ]; then
+    verbose=1
+fi
+
 _test() {
     x=`python user_friendly_url.py "$1"`
+
+    if [ ! -z $verbose ]; then
+        echo "sent:     $1"
+        echo "expected: $2"
+        echo "got:      $x"
+    fi
 
     if [ "x$x" == "x$2" ]; then
         echo OK
@@ -12,8 +22,12 @@ _test() {
 
 _test \
     "http://xn--80a1acny.xn--p1ai/resp_engine.aspx?Path=RP/INDEX/RU/Home/Search&keyword=%D0%BF%D1%80%D0%B8%D0%B2%D0%B5%D1%82" \
-    "почта.рф/resp_engine.aspx?Path=RP/INDEX/RU/Home/Search&keyword=привет"
+    "http://почта.рф/resp_engine.aspx?Path=RP/INDEX/RU/Home/Search&keyword=привет"
 
 _test \
     "ftp://xn--80a1acny.xn--p1ai/" \
-    "ftp://почта.рф"
+    "ftp://почта.рф/"
+
+_test \
+    "xn--80a1acny.xn--p1/%D0%BF%D1%80%D0%B8%D0%B2%D0%B5%D1%82" \
+    "xn--80a1acny.xn--p1/привет"
--- user_friendly_url.py
+++ user_friendly_url.py
@@ -1,49 +1,20 @@
-import re
 import sys
+import urllib
 import urlparse
 
 cache = {}
 
 def decode_iri_nocache(iri):
-    protocol, host, path, query, fragment = list(urlparse.urlsplit(iri))
+    iri = urllib.unquote_plus(iri)
+    r = urlparse.urlsplit(iri)
 
-    try: host = host.decode('idna').encode('UTF-8') #punycode to utf-8
-    except: pass
+    try:
+        host = r.hostname.decode('idna').encode('utf8')
+        iri = iri.replace(r.hostname, host)
+    except:
+        pass
 
-    #decode urlencode to utf-8, if possible
-    def urldecode(s):
-        if '%' not in s:
-            return s
-        try:
-            def replace_if_url_safe_symbol(match):
-                txt = match.group(0)
-                c = chr(int(txt[1:], 16))
-                if ord(c) >= 128 or c in ''' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"\'()*-.:<>[]^`{|}~''':
-                    return c
-                else:
-                    return txt
-            s2 = re.sub('%[0-9A-Fa-f]{2}', replace_if_url_safe_symbol, s)
-            s2 = s2.decode('UTF-8').encode('UTF-8') #check, is valid UTF-8
-            return s2
-        except UnicodeError:
-            return s
-
-    path = urldecode(path)
-    query = urldecode(query)
-    fragment = urldecode(fragment)
-
-    result = host
-    if protocol and protocol.lower() != "http":
-        result = protocol + "://" + result
-    after_host = path
-    if query:
-        after_host += '?' + query
-    if fragment:
-        after_host += '#' + fragment
-    if after_host != '/':
-        result += after_host
-
-    return result
+    return iri
 
 def decode_iri(iri):
     return cache.setdefault(iri, decode_iri_nocache(iri))
